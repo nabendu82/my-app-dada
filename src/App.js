238b22c5c119cdcef4ca473c59efec0c6df5858b
nabendu82 <nabendu.biswas@gmail.com>
2022-05-13 07:38:46 +0530

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import { Greet, ReactGreet } from './components/Greet';
-import Greeting from './components/Greeting';
-import Welcome from './components/Welcome';
+import EventBind from './components/EventBind';
+// import Counter from './components/Counter';
+// import { Greet, ReactGreet } from './components/Greet';
+// import Greeting from './components/Greeting';
+// import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <ReactGreet />
-      <Greeting />
-      <Welcome />
+      {/* <Greet />
+      <ReactGreet /> */}
+      {/* <Greeting name="JavaScript" />
+      <Greeting name="React" />
+      <Greeting name="React Native">Language for app development</Greeting>
+      <hr />
+      <Welcome name="Flutter" />
+      <Welcome name="Golang" />
+      <Welcome name="Java"><button>Click</button></Welcome> */}
+      {/* <Counter /> */}
+      <EventBind />
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 
 class Welcome extends Component {
     render() {
+        const { name, children } = this.props;
         return (
-            <h1>Welcome to React</h1>
+            <>
+                <h1>Welcome {name}</h1>
+                <p>{children}</p>
+                {/* <label htmlFor="name">Name </label>
+                <input type="text" id="name" /> */}
+            </>
         )
     }
 }
